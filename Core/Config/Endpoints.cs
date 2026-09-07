9cb08ceb31371f87915b308f6344d2142af688e5
Gredja <gredja.a@gmail.com>
2026-09-07 08:28:41 +0300

    Add product sort endpoint

--- Core/Config/Endpoints.cs
+++ Core/Config/Endpoints.cs
@@ -17,4 +17,6 @@ public static class Endpoints
     public const string UsersById = "/users/{id}";
 
     public const string Login = "/auth/login";
+
+    public const string ProductsSort = "/products?sort={sort}";
 }
